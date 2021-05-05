2e89268638864b9e5254d2a17a08882a05eeb4cf
Naman Vrati <info@namanvrati.cf>
2021-05-06 01:06:24 +0530

    Basic event handler

--- bot.js
+++ bot.js
@@ -3,6 +3,17 @@ const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
+
 client.commands = new Discord.Collection();
 client.cooldowns = new Discord.Collection();
 
