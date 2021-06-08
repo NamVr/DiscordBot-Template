41b5c35739c2948f784dfbf0a628123dee01c872
Naman Vrati <info@namanvrati.cf>
2021-06-08 17:17:38 +0530

    Modified handler with mention prefix and message.js seperation, with more detailed code commented.

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -1,6 +1,9 @@
 module.exports = {
 	name: 'ping',
-	description: 'Ping!',
+	//description: 'Ping!',
+	//usage: 'put usage here',
+	//permissions: 'SEND_MESSAGES',
+	//guildOnly: true,
 	execute(message, args) {
 		message.channel.send('Pong.');
 	},
